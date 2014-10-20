7f5a542de5149fa3a2422dc064f298fe574cb71d
JT Olds <hello@jtolds.com>
2014-10-19 23:35:02 -0600

    Update doc.go

--- trace/doc.go
+++ trace/doc.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/* trace is a distributed tracing and Zipkin client library for Go.
+/*
+Package trace is a distributed tracing and Zipkin client library for Go.
 
 Background
 
