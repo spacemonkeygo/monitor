ca52439c370fa285692e81c86319bb5aaa9de7f2
JT Olds <hello@jtolds.com>
2014-11-21 01:57:29 -0700

    monitor: make running task output more like ps

--- http.go
+++ http.go
@@ -22,11 +22,19 @@ import (
 	"time"
 )
 
-type durationSort []time.Duration
+type sortableTask struct {
+	name    string
+	elapsed time.Duration
+}
 
-func (d durationSort) Len() int           { return len(d) }
-func (d durationSort) Less(i, j int) bool { return d[i] < d[j] }
-func (d durationSort) Swap(i, j int)      { d[i], d[j] = d[j], d[i] }
+type sortableTasks []sortableTask
+
+func (s sortableTasks) Len() int      { return len(s) }
+func (s sortableTasks) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+func (s sortableTasks) Less(i, j int) bool {
+	return s[i].elapsed < s[j].elapsed
+}
 
 // ServeHTTP dumps all of the MonitorStore's keys and values to the requester.
 // This method allows a MonitorStore to be registered as an HTTP handler.
@@ -34,17 +42,18 @@ func (s *MonitorStore) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 
 	if strings.HasSuffix(req.URL.Path, "running") {
+		var tasks []sortableTask
 		s.Running(func(name string, current []*TaskCtx) {
-			fmt.Fprintf(w, "%s - %d tasks\n", name, len(current))
-			durs := make([]time.Duration, 0, len(current))
 			for _, task := range current {
-				durs = append(durs, task.ElapsedTime())
-			}
-			sort.Sort(sort.Reverse(durationSort(durs)))
-			for _, dur := range durs {
-				fmt.Fprintf(w, "\t%s\n", dur)
+				tasks = append(tasks, sortableTask{
+					name:    name,
+					elapsed: task.ElapsedTime()})
 			}
 		})
+		sort.Sort(sort.Reverse(sortableTasks(tasks)))
+		for _, task := range tasks {
+			fmt.Fprintf(w, "%s\t%s\n", task.elapsed, task.name)
+		}
 		return
 	}
 
